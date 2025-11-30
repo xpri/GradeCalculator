cdee352757d2bdd78d89899aee3a50fd2322a597
James Aspilcueta <jaspil007@gmail.com>
2025-11-30 00:29:16 -0500

    Convert unnecessary functions from main into helper files.

---
+++ FINAL_GradeCalculator/src/helper.cpp
@@ -0,0 +1,48 @@
+#include "helper.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+void GradeCalculator()
+{
+    std::vector<std::string> names_of_categories;
+    std::vector<int> weights_of_categories;
+
+    std::string course_name;
+    std::cout << "Please enter the class name that you are calculating your grade for: ";
+    std::getline(std::cin, course_name);
+    std::cout << "Great! Calculator for course: " << course_name << " created!\n" << std::endl;
+
+    int num_of_categories;
+    std::cout << "How many weights are there for each grading category (e.g. 3 if you have homework, exam, and a final project)" << std::endl;
+    std::cin >> num_of_categories;
+    std::cin.ignore();
+    std::cout << std::endl;
+
+    for (int i = 0; i < num_of_categories; i++)
+    {
+        int temp_for_weigths;
+        std::string temp_for_names;
+        std::cout << "Enter category " << i + 1 << ": ";
+        std::getline(std::cin, temp_for_names);
+
+        std::cout << "Enter weight (in percentage) " << i + 1 << ": ";
+        std::cin >> temp_for_weigths;
+        std::cin.ignore();
+        
+        weights_of_categories.push_back(temp_for_weigths);
+        names_of_categories.push_back(temp_for_names);
+    }
+
+    for (int i = 0; i < names_of_categories.size(); i++)
+    {
+        std::cout << "How many different items are in" << names_of_categories[i] << "?" << std::endl;
+    }
+}
+
+void printSectionHeader(const std::string& title)
+{
+    std::cout << std::string(60, '=') << std::endl;
+    std::cout << title << std::endl;
+    std::cout << std::string(60, '=') << std::endl;
+}
---
+++ FINAL_GradeCalculator/src/helper.h
@@ -0,0 +1,11 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+void GradeCalculator();
+void printSectionHeader(const std::string& title);
+
+#endif // HELPER_H
